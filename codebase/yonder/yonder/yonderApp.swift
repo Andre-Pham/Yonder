331ef9641c6a1da42ce335704bb93fc86d21a186
Andre Pham <andrepham33@gmail.com>
2021-11-17 05:59:32 +1100

    Initial commit

---
+++ codebase/yonder/yonder/yonderApp.swift
@@ -0,0 +1,17 @@
+//
+//  yonderApp.swift
+//  yonder
+//
+//  Created by Andre Pham on 17/11/21.
+//
+
+import SwiftUI
+
+@main
+struct yonderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
