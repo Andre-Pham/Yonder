e27596bee785d43113f2f205c4ae6c4a82870211
Andre Pham <andrepham33@gmail.com>
2021-12-11 09:38:53 +1100

    Added a sheet just to try out future implementation

--- codebase/yonder/yonder/UI/Options/PlayerView.swift
+++ codebase/yonder/yonder/UI/Options/PlayerView.swift
@@ -9,43 +9,51 @@ import SwiftUI
 
 struct PlayerView: View {
     var player = Player(maxHealth: 200, location: NoLocation())
+    @State private var showingPlayerSheet = false
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 3) {
-            Text("You")
-                .font(YonderFonts.main())
-                .padding(.top)
+        Button {
+            showingPlayerSheet.toggle()
+        } label: {
+            VStack(alignment: .leading, spacing: 3) {
+                Text("You")
+                    .font(YonderFonts.main())
+                    .padding(.top)
+                    .padding(.leading)
+                    .padding(.trailing)
+                
+                HStack {
+                    Text("450")
+                        .font(YonderFonts.main(size: 26))
+                    
+                    Spacer()
+                    
+                    Text("/500")
+                        .font(YonderFonts.main(size: 18))
+                }
                 .padding(.leading)
                 .padding(.trailing)
-            
-            HStack {
-                Text("450")
-                    .font(YonderFonts.main(size: 26))
+                
+                HStack {
+                    Text("100")
+                        .font(YonderFonts.main(size: 26))
+                    
+                    Spacer()
+                    
+                    Text("/100")
+                        .font(YonderFonts.main(size: 18))
+                }
+                .padding(.leading)
+                .padding(.trailing)
                 
                 Spacer()
-                
-                Text("/500")
-                    .font(YonderFonts.main(size: 18))
             }
-            .padding(.leading)
-            .padding(.trailing)
-            
-            HStack {
-                Text("100")
-                    .font(YonderFonts.main(size: 26))
-                
-                Spacer()
-                
-                Text("/100")
-                    .font(YonderFonts.main(size: 18))
-            }
-            .padding(.leading)
-            .padding(.trailing)
-            
-            Spacer()
+            .foregroundColor(.Yonder.textMaxContrast)
+            .background(Color.Yonder.backgroundMinDepth)
         }
-        .foregroundColor(.Yonder.textMaxContrast)
-        .background(Color.Yonder.backgroundMinDepth)
+        .sheet(isPresented: $showingPlayerSheet) {
+            Text("Wow!")
+        }
     }
 }
 
