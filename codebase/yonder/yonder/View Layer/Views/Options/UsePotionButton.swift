1aa52fe8bc074c8a8c9b8fbef193edca635897f9
Andre Pham <andrepham33@gmail.com>
2022-04-15 20:22:38 +1000

    Changed UsePotionButton to make use of YonderExpandableWideButtonBody

--- codebase/yonder/yonder/View Layer/Views/Options/UsePotionButton.swift
+++ codebase/yonder/yonder/View Layer/Views/Options/UsePotionButton.swift
@@ -14,47 +14,25 @@ struct UsePotionButton: View {
     
     // Note: animation needs to be disabled when "use" isn't active, otherwise whenever the view updates, all buttons re-animate to their value
     var body: some View {
-        ZStack(alignment: .bottom) {
-            YonderWideButtonBody {
-                self.useButtonActive.toggle()
+        YonderExpandableWideButtonBody(
+            isExpanded: self.$useButtonActive,
+            expandedButtonText: Term.instantUse.capitalized) {
+                self.playerViewModel.use(potionViewModel: self.potionViewModel)
             } label: {
-                VStack {
-                    HStack {
-                        VStack(alignment: .leading) {
-                            YonderText(text: self.potionViewModel.name, size: .buttonBody)
-                                .padding(.bottom, YonderCoreGraphics.buttonTitleSpacing)
-                            
-                            YonderText(text: self.potionViewModel.description, size: .buttonBodySubscript)
-                            
-                            YonderTextAndNumeral(
-                                format: [.numeral, .text],
-                                text: [" " + (self.potionViewModel.remainingUses > 1 ? Term.potions.capitalized : Term.potion.capitalized)],
-                                numbers: [self.potionViewModel.remainingUses],
-                                size: .buttonBodySubscript,
-                                animationIsActive: self.useButtonActive)
-                        }
-                        
-                        Spacer()
-                    }
-                    .padding(.horizontal, YonderCoreGraphics.padding)
+                VStack(alignment: .leading) {
+                    YonderText(text: self.potionViewModel.name, size: .buttonBody)
+                        .padding(.bottom, YonderCoreGraphics.buttonTitleSpacing)
                     
-                    if self.useButtonActive {
-                        // Expand button frame
-                        YonderWideButton(text: "") {}
-                        .padding(.top, YonderCoreGraphics.padding)
-                        .hidden()
-                    }
+                    YonderText(text: self.potionViewModel.description, size: .buttonBodySubscript)
+                    
+                    YonderTextAndNumeral(
+                        format: [.numeral, .text],
+                        text: [" " + (self.potionViewModel.remainingUses > 1 ? Term.potions.capitalized : Term.potion.capitalized)],
+                        numbers: [self.potionViewModel.remainingUses],
+                        size: .buttonBodySubscript,
+                        animationIsActive: self.useButtonActive)
                 }
             }
-            
-            if self.useButtonActive {
-                YonderWideButton(text: Term.instantUse.capitalized) {
-                    self.playerViewModel.use(potionViewModel: self.potionViewModel)
-                }
-                .padding(.horizontal, YonderCoreGraphics.padding)
-                .padding(.bottom, YonderCoreGraphics.padding)
-            }
-        }
     }
 }
 
