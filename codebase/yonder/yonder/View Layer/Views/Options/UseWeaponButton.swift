aa8025c11484f34a19e6f0d4670747fcbf65a6bd
Andre Pham <andrepham33@gmail.com>
2022-04-15 20:21:58 +1000

    Adjusted indentation in UseWeaponButton

--- codebase/yonder/yonder/View Layer/Views/Options/UseWeaponButton.swift
+++ codebase/yonder/yonder/View Layer/Views/Options/UseWeaponButton.swift
@@ -18,37 +18,37 @@ struct UseWeaponButton: View {
             isExpanded: self.$useButtonActive,
             expandedButtonText: Term.use.capitalized) {
                 self.playerViewModel.use(weaponViewModel: self.weaponViewModel)
-        } label: {
-            VStack(alignment: .leading) {
-                YonderText(text: self.weaponViewModel.name, size: .buttonBody)
-                    .padding(.bottom, YonderCoreGraphics.buttonTitleSpacing)
-                
-                if self.weaponViewModel.damage > 0 {
+            } label: {
+                VStack(alignment: .leading) {
+                    YonderText(text: self.weaponViewModel.name, size: .buttonBody)
+                        .padding(.bottom, YonderCoreGraphics.buttonTitleSpacing)
+                    
+                    if self.weaponViewModel.damage > 0 {
+                        YonderTextAndNumeral(
+                            format: [.numeral, .text],
+                            text: [" " + Term.damage.capitalized],
+                            numbers: [self.weaponViewModel.damage],
+                            size: .buttonBodySubscript,
+                            animationIsActive: self.useButtonActive)
+                    }
+                    
+                    if self.weaponViewModel.healthRestoration > 0 {
+                        YonderTextAndNumeral(
+                            format: [.numeral, .text],
+                            text: [" " + Term.healthRestoration.capitalized],
+                            numbers: [self.weaponViewModel.healthRestoration],
+                            size: .buttonBodySubscript,
+                            animationIsActive: self.useButtonActive)
+                    }
+                    
                     YonderTextAndNumeral(
                         format: [.numeral, .text],
-                        text: [" " + Term.damage.capitalized],
-                        numbers: [self.weaponViewModel.damage],
+                        text: [" " + (self.weaponViewModel.remainingUses > 1 ? Term.remainingUses.capitalized : Term.remainingUse.capitalized)],
+                        numbers: [self.weaponViewModel.remainingUses],
                         size: .buttonBodySubscript,
                         animationIsActive: self.useButtonActive)
                 }
-                
-                if self.weaponViewModel.healthRestoration > 0 {
-                    YonderTextAndNumeral(
-                        format: [.numeral, .text],
-                        text: [" " + Term.healthRestoration.capitalized],
-                        numbers: [self.weaponViewModel.healthRestoration],
-                        size: .buttonBodySubscript,
-                        animationIsActive: self.useButtonActive)
-                }
-                
-                YonderTextAndNumeral(
-                    format: [.numeral, .text],
-                    text: [" " + (self.weaponViewModel.remainingUses > 1 ? Term.remainingUses.capitalized : Term.remainingUse.capitalized)],
-                    numbers: [self.weaponViewModel.remainingUses],
-                    size: .buttonBodySubscript,
-                    animationIsActive: self.useButtonActive)
             }
-        }
     }
 }
 
