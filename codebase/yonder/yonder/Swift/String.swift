a5616f20ddb1eb2d330afed5143ad836fd1d69fd
Andre Pham <andrepham33@gmail.com>
2022-06-27 15:08:36 +1000

    Implemented IndicativeEffectsDescriptionView

--- codebase/yonder/yonder/Swift/String.swift
+++ codebase/yonder/yonder/Swift/String.swift
@@ -9,6 +9,69 @@ import Foundation
 
 extension String {
     
+    /// Retrieves the first available Int in a string.
+    /// Example:
+    /// ``` "a10.b20.0".parseFirstInt() -> 10
+    ///     ".x002.50".parseFirstInt() -> 2
+    /// ```
+    /// - Returns: First Int in a string.
+    func parseFirstInt() -> Int? {
+        let components = self.components(separatedBy: CharacterSet.decimalDigits.inverted)
+        for component in components {
+            if let firstInt = Int(component) {
+                return firstInt
+            }
+        }
+        return nil
+    }
+    
+    /// Retrieves the first available and valid Double in a string.
+    /// Example:
+    /// ``` "a10.b20.0".parseFirstDouble() -> 20.0
+    ///     ".1b50a01.0".parseFirstDouble() -> 1.0
+    /// ```
+    /// - Returns: First Double in a string that must include a decimal point padded by numbers.
+    func parseFirstDouble() -> Double? {
+        var charSet = CharacterSet.decimalDigits
+        charSet.insert(".")
+        let components = self.components(separatedBy: charSet.inverted)
+        for component in components {
+            let isValidDouble = (component.contains(".") &&
+                                 component.first != "." &&
+                                 component.last != ".")
+            if let firstDouble = Double(component), isValidDouble {
+                return firstDouble
+            }
+        }
+        return nil
+    }
+    
+    /// Separates a string by an Int embedded in the string. The int must occur only once.
+    /// Example:
+    /// ``` "I am 20 years old.".separatedBy(20) -> ("I am ", " years old.")
+    /// ```
+    /// - Returns: Two strings in a tuple, first from the left side of the Int argument, second from the right side.
+    func separatedBy(int: Int) -> (String, String)? {
+        let components = self.components(separatedBy: String(int))
+        if components.count == 2 {
+            return (components[0], components[1])
+        }
+        return nil
+    }
+    
+    /// Separates a string by a Double embedded in the string.
+    /// Example:
+    /// ``` "a10.b20.0x".separatedBy(20.0) -> ("a10.b", "x")
+    /// ```
+    /// - Returns: Two strings in a tuple, first from the left side of the Double argument, second from the right side.
+    func separatedBy(double: Double) -> (String, String)? {
+        let components = self.components(separatedBy: String(double))
+        if components.count == 2 {
+            return (components[0], components[1])
+        }
+        return nil
+    }
+    
     func lowercaseFirst() -> String {
         if let first = self.first {
             return first.lowercased() + self.dropFirst()
---
+++ codebase/yonder/yonder/View Layer/Views/Options/IndicativeEffectsDescriptionView.swift
@@ -0,0 +1,49 @@
+//
+//  IndicativeEffectsDescriptionView.swift
+//  yonder
+//
+//  Created by Andre Pham on 27/6/2022.
+//
+
+import SwiftUI
+
+struct IndicativeEffectsDescriptionView: View {
+    let effectsDescription: String
+    let indicative: Int
+    let size: YonderTextSize
+    var color: Color = YonderColors.textMaxContrast
+    
+    var body: some View {
+        if let original = self.effectsDescription.parseFirstDouble(), let strings = self.effectsDescription.separatedBy(double: original) {
+            YonderTextNumeralHStack {
+                YonderText(text: strings.0, size: self.size, color: self.color)
+                
+                IndicativeNumeralView(original: Int(round(original)), indicative: self.indicative, size: self.size, leftPadding: 0.0)
+                
+                YonderText(text: strings.1, size: self.size, color: self.color)
+            }
+        } else if let original = self.effectsDescription.parseFirstInt(), let strings = self.effectsDescription.separatedBy(int: original) {
+            // Int has to go second in an else statement, otherwise "20.0%" would register 20 as an Int
+            YonderTextNumeralHStack {
+                YonderText(text: strings.0, size: self.size, color: self.color)
+                
+                IndicativeNumeralView(original: original, indicative: self.indicative, size: self.size, leftPadding: 0.0)
+                
+                YonderText(text: strings.1, size: self.size, color: self.color)
+            }
+        } else {
+            YonderText(text: self.effectsDescription, size: self.size, color: self.color)
+        }
+    }
+}
+
+struct IndicativeEffectsDescriptionView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            YonderColors.backgroundMaxDepth
+                .ignoresSafeArea()
+            
+            IndicativeEffectsDescriptionView(effectsDescription: "Inflicts 100 damage.", indicative: 250, size: .buttonBody)
+        }
+    }
+}
