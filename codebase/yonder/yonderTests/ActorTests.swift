0aced7514ac27201fe8e6299a90ed43905cf60c6
Andre Pham <andrepham33@gmail.com>
2022-09-06 14:35:57 +1000

    Added testRestorationAdjusted

--- codebase/yonder/yonderTests/ActorTests.swift
+++ codebase/yonder/yonderTests/ActorTests.swift
@@ -35,5 +35,16 @@ class ActorTests: XCTestCase {
         XCTAssertEqual(self.actor.maxHealth, 300)
         XCTAssertEqual(self.actor.health, 200)
     }
+    
+    func testRestorationAdjusted() throws {
+        self.actor.equipArmor(Armor(name: "", description: "", type: .body, armorPoints: 500, basePurchasePrice: 10, armorBuffs: [], equipmentPills: []))
+        self.actor.damage(for: 600) // Set to 100/200 health, 0/500 armor
+        self.actor.addBuff(HealthRestorationPercentBuff(sourceName: "", direction: .incoming, duration: nil, healthFraction: 2.0))
+        self.actor.addBuff(ArmorPointsRestorationPercentBuff(sourceName: "", direction: .incoming, duration: nil, armorPointsFraction: 3.0))
+        self.actor.restoreAdjusted(sourceOwner: self.actor, using: NoItem(), for: 100)
+        // 100 restoration. 50 is required for health since healing is doubled. The remaining 50 is tripled for armor points.
+        XCTAssertEqual(self.actor.health, 200)
+        XCTAssertEqual(self.actor.armorPoints, 150)
+    }
 
 }
