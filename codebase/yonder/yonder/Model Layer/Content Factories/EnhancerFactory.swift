3b018ea5d0912808c95ddd0b6497b0c9e29b84b2
Andre Pham <andrepham33@gmail.com>
2022-11-27 21:09:36 +1100

    Added enhance offers to enhancer factory

--- codebase/yonder/yonder/Model Layer/Content Factories/EnhancerFactory.swift
+++ codebase/yonder/yonder/Model Layer/Content Factories/EnhancerFactory.swift
@@ -25,17 +25,68 @@ class EnhancerFactory {
         let offerCount = Int.random(in: 2...4)
         while offers.count < offerCount {
             // Order doesn't affect outcome because the loop doesn't immediately break if the offer count is reached
-            // We can't just add lots of each and shuffle because that may cause many duplicate enhance offers
+            // It's less optimal to add lots of each and shuffle because that can cause many duplicate enhance offers
             
             // 01
-            if Random.roll(1, in: 10) {
+            if Random.roll(1, in: 5) {
                 offers.append(EnhanceOffers.damageForWeapon(stage: stage))
             }
             // 02
-            if Random.roll(1, in: 30) {
+            if Random.roll(1, in: 15) {
                 offers.append(EnhanceOffers.lifestealForWeapon(stage: stage))
             }
-            // TODO: Add remaining enhance offers
+            // 03
+            if Random.roll(1, in: 15) {
+                offers.append(EnhanceOffers.burnForWeapon(stage: stage))
+            }
+            // 04
+            if Random.roll(1, in: 5) {
+                offers.append(EnhanceOffers.durabilityForWeapon(stage: stage))
+            }
+            // 05
+            if Random.roll(1, in: 15) {
+                offers.append(EnhanceOffers.growDamageForWeapon(stage: stage))
+            }
+            // 06
+            if Random.roll(1, in: 10) {
+                offers.append(EnhanceOffers.resistanceForArmor(stage: stage))
+            }
+            // 07
+            if Random.roll(1, in: 10) {
+                offers.append(EnhanceOffers.damagePercentForArmor(stage: stage))
+            }
+            // 08
+            if Random.roll(1, in: 5) {
+                offers.append(EnhanceOffers.armorPointsForArmor(stage: stage))
+            }
+            // 09
+            if Random.roll(1, in: 15) {
+                offers.append(EnhanceOffers.thornsForArmor(stage: stage))
+            }
+            // 10
+            if Random.roll(1, in: 15) {
+                offers.append(EnhanceOffers.bonusHealthForAccessory(stage: stage))
+            }
+            // 11
+            if Random.roll(1, in: 15) {
+                offers.append(EnhanceOffers.phoenixForEquipment(stage: stage))
+            }
+            // 12
+            if Random.roll(1, in: 20) {
+                offers.append(EnhanceOffers.doubleHealthRestorationForWeapon(stage: stage))
+            }
+            // 13
+            if Random.roll(1, in: 20) {
+                offers.append(EnhanceOffers.doubleArmorPointsRestorationForWeapon(stage: stage))
+            }
+            // 14
+            if Random.roll(1, in: 15) {
+                offers.append(EnhanceOffers.goldPercentBuffForAccessory(stage: stage))
+            }
+            // 15
+            if Random.roll(1, in: 15) {
+                offers.append(EnhanceOffers.pricePercentBuffForAccessory(stage: stage))
+            }
         }
         if offers.count > offerCount {
             offers.shuffle()
