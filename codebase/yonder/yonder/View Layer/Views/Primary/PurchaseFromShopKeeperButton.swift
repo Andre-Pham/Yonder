5a50130cc18b27199ca8bc4e1578affa7c143bfe
Andre Pham <andrepham33@gmail.com>
2022-12-08 02:45:38 +1100

    Fixed purchasing accessory selection bug

--- codebase/yonder/yonder/View Layer/Views/Primary/PurchaseFromShopKeeperButton.swift
+++ codebase/yonder/yonder/View Layer/Views/Primary/PurchaseFromShopKeeperButton.swift
@@ -80,7 +80,7 @@ struct PurchaseFromShopKeeperButton: View {
             
             if self.useButtonActive {
                 YonderWideButton(text: Strings("button.purchase").local) {
-                    if let accessory = self.purchasableViewModel.getAccessoryViewModel(), accessory.isPeripheral {
+                    if let accessory = self.purchasableViewModel.getAccessoryViewModel(), !accessory.isPeripheral {
                         self.accessorySelectionSheetActive = true
                     } else {
                         self.purchasableViewModel.purchase(by: self.playerViewModel, amount: self.amount)
