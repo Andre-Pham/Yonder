00bf8d4199ddafa2cd8d8ed198755e816807ae1e
Andre Pham <andrepham33@gmail.com>
2023-01-17 11:54:46 +1100

    Removed redundant code

--- codebase/yonder/yonder/Model Layer/Content Factories/TerritoryPoolFactory.swift
+++ codebase/yonder/yonder/Model Layer/Content Factories/TerritoryPoolFactory.swift
@@ -9,27 +9,19 @@ import Foundation
 
 class TerritoryPoolFactory {
     
-    private let accessoryProfileBucket = AccessoryProfileBucket()
-    private let armorProfileBucket = ArmorProfileBucket()
-    private let weaponProfileBucket = WeaponProfileBucket()
-    private let foeProfileBucket = FoeProfileBucket()
-    private let shopKeeperProfileBucket = ShopKeeperProfileBucket()
-    private let enhancerProfileBucket = EnhancerProfileBucket()
-    private let restorerProfileBucket = RestorerProfileBucket()
-    private let friendlyProfileBucket = FriendlyProfileBucket()
-    private let areaProfileBucket = RegionProfileBucket()
+    private let regionProfileBucket = RegionProfileBucket()
     
     func deliver(stage: Int) -> TerritoryPool {
         var areaPools = [AreaPool]()
         var tavernAreas = [TavernArea]()
         
-        let areaProfiles = self.areaProfileBucket.grabProfiles(count: 2, stage: stage)
+        let areaProfiles = self.regionProfileBucket.grabProfiles(count: 2, stage: stage)
         for areaProfile in areaProfiles {
             let areaPoolFactory = AreaPoolFactory(areaProfile: areaProfile)
             areaPools.append(areaPoolFactory.deliver())
         }
         
-        let tavernAreaProfile = self.areaProfileBucket.grabProfile(stage: stage)
+        let tavernAreaProfile = self.regionProfileBucket.grabProfile(stage: stage)
         let tavernAreaFactory = TavernAreaFactory(areaProfile: tavernAreaProfile, stage: stage)
         tavernAreas.append(tavernAreaFactory.deliver())
         
