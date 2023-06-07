19af7b699bd089821ecc282c71e660a07b6bba80
Andre Pham <andrepham33@gmail.com>
2023-06-08 08:50:36 +1000

    AnimationApp init

---
+++ temp/Animation/Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Andre Pham on 8/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
