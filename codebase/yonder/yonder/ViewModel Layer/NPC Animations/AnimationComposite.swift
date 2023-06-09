7ce6acb2f23a6b75cc0df32be9830d3deec7f905
Andre Pham <andrepham33@gmail.com>
2023-06-09 13:47:58 +1000

    Created AnimationManager and AnimationSequenceBuilder

--- codebase/yonder/yonder/ViewModel Layer/NPC Animations/AnimationComposite.swift
+++ codebase/yonder/yonder/ViewModel Layer/NPC Animations/AnimationComposite.swift
@@ -6,3 +6,99 @@
 //
 
 import Foundation
+import SwiftUI
+import SwiftyJSON
+
+class AnimationManager: ObservableObject, SequenceDelegate {
+    
+    private static let SPRITE_SHEET_PREFIX = "IMG-"
+    private static let FRAMES_DATA_PREFIX = "FRAMES-"
+    
+    @Published private(set) var frame: Image
+    private var sequences = [String: AnimationSequence]()
+    private var playbackSpeed = 1.0
+    private(set) var activeSequenceCode: SequenceCode
+    private var activeSequence: AnimationSequence {
+        return self.getSequence(self.activeSequenceCode)
+    }
+    public var isPlaying: Bool {
+        return self.activeSequence.isPlaying
+    }
+    
+    init?(fileID: String, initialSequence: SequenceCode) {
+        guard let spriteSheet = UIImage(named: Self.SPRITE_SHEET_PREFIX + fileID) else {
+            assertionFailure("Animation ID provided doesn't have corresponding sprite sheet")
+            return nil
+        }
+        guard let framesURL = Bundle.main.url(forResource: Self.FRAMES_DATA_PREFIX + fileID, withExtension: "json"),
+              let framesData = try? Data(contentsOf: framesURL) else {
+            assertionFailure("Frames data could not be loaded")
+            return nil
+        }
+        guard let framesDataJSON = try? JSON(data: framesData) else {
+            assertionFailure("JSON could not be retrieved from frames data")
+            return nil
+        }
+        let builder = AnimationSequenceBuilder(spriteSheet: spriteSheet, framesDataJSON: framesDataJSON)
+        for code in SequenceCode.allCases {
+            guard let sequence = builder.build(prefix: code.rawValue) else {
+                assertionFailure("Sequence(s) couldn't be built")
+                return nil
+            }
+            self.sequences[code.rawValue] = sequence
+        }
+        self.activeSequenceCode = initialSequence
+        self.frame = self.sequences[self.activeSequenceCode.rawValue]!.frame
+        for code in SequenceCode.allCases {
+            self.getSequence(code).setDelegate(to: self)
+        }
+        self.setupSequences()
+    }
+    
+    func setupSequences() {
+        self.getSequence(.death).setLoopBehaviour(to: false)
+        self.getSequence(.attack).setLoopBehaviour(to: false)
+    }
+    
+    private func getSequence(_ code: SequenceCode) -> AnimationSequence {
+        return self.sequences[code.rawValue]!
+    }
+    
+    func setPlaybackSpeed(to speed: Double) {
+        self.playbackSpeed = speed
+        self.activeSequence.setPlaybackSpeed(to: self.playbackSpeed)
+    }
+    
+    func setSequence(to code: SequenceCode) {
+        self.activeSequence.reset()
+        self.activeSequenceCode = code
+        self.activeSequence.setPlaybackSpeed(to: self.playbackSpeed)
+        self.frame = self.activeSequence.frame
+        self.activeSequence.play()
+    }
+    
+    func onNewFrame(_ frame: Image) {
+        self.frame = frame
+    }
+    
+    func onSequenceEnd() {
+        // Blank
+    }
+    
+    func play() {
+        self.activeSequence.play()
+    }
+    
+    func pause() {
+        self.activeSequence.pause()
+    }
+    
+    func reset() {
+        self.activeSequence.reset()
+    }
+    
+    func restart() {
+        self.activeSequence.restart()
+    }
+    
+}
--- codebase/yonder/yonder/ViewModel Layer/NPC Animations/AnimationSequenceBuilder.swift
+++ codebase/yonder/yonder/ViewModel Layer/NPC Animations/AnimationSequenceBuilder.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+import UIKit
+import SwiftyJSON
+
+class AnimationSequenceBuilder {
+    
+    private let spriteSheet: UIImage
+    private let framesDataJSON: JSON
+    
+    init(spriteSheet: UIImage, framesDataJSON: JSON) {
+        self.spriteSheet = spriteSheet
+        self.framesDataJSON = framesDataJSON
+    }
+    
+    func build(prefix: String) -> AnimationSequence? {
+        guard let frameDuration = self.framesDataJSON["\(prefix)_frame_duration"].double else {
+            assertionFailure("Frame duration could not be read from JSON")
+            return nil
+        }
+        guard let frameOriginsJSON = self.framesDataJSON["\(prefix)_coords"].array else {
+            assertionFailure("Frame origins could not be read from JSON")
+            return nil
+        }
+        var frameOrigins = [SpriteSheetCoord]()
+        for originJSON in frameOriginsJSON {
+            guard let x = originJSON["x"].int, let y = originJSON["y"].int else {
+                assertionFailure("Origin coordinate could not be read from JSON")
+                return nil
+            }
+            frameOrigins.append(SpriteSheetCoord(x: x, y: y))
+        }
+        guard let frameWidth = self.framesDataJSON["frame_width"].int,
+              let frameHeight = self.framesDataJSON["frame_height"].int else {
+            assertionFailure("Frame width/height could not be read from JSON")
+            return nil
+        }
+        return AnimationSequence(
+            spriteSheet: self.spriteSheet,
+            frameDuration: frameDuration,
+            frameOrigins: frameOrigins,
+            frameSize: (width: frameWidth, height: frameHeight)
+        )
+    }
+    
+}
