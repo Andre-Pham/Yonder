59f9f6856927710b105074a170ffcd27489f0cd1
Andre Pham <andrepham33@gmail.com>
2023-06-26 02:30:21 +1000

    Fixed unused self warning in timer

--- codebase/yonder/yonder/ViewModel Layer/NPC Animations/SequenceTimer.swift
+++ codebase/yonder/yonder/ViewModel Layer/NPC Animations/SequenceTimer.swift
@@ -32,7 +32,8 @@ class SequenceTimer {
     ///   - callback: The callback to trigger at every time interval
     func start(withTimeInterval timeInterval: TimeInterval, callback: @escaping () -> Void) {
         self.stop()
-        self.timer = Timer(timeInterval: timeInterval, repeats: true) { [weak self] _ in
+        // Note: Uncomment [weak self] if referring to self within timer closure
+        self.timer = Timer(timeInterval: timeInterval, repeats: true) { /*[weak self]*/ _ in
             callback()
         }
         // Keep the timer running outside of the view lifecycle
