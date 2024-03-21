628e99c0ff83e63e99a99cd6da92680ac2f0f193
Andre Pham <andrepham33@gmail.com>
2024-03-21 14:32:07 +1100

    Player health is now red when in critical health

--- codebase/yonder/yonder/View Layer/Views/Primary/Card Views/PlayerCardView.swift
+++ codebase/yonder/yonder/View Layer/Views/Primary/Card Views/PlayerCardView.swift
@@ -10,24 +10,36 @@ import SwiftUI
 struct PlayerCardView: View {
     @ObservedObject var playerViewModel: PlayerViewModel
     var resizeToFit: Bool = true
+    private var playerHasCriticalHealth: Bool {
+        if let foe = GameManager.instance.foeViewModel {
+            return self.playerViewModel.hasCriticalHealth(against: foe)
+        }
+        return false
+    }
     
     var body: some View {
-        CardBody(name: Strings("card.player.name").local,
-                 resizeToFit: self.resizeToFit) {
+        CardBody(
+            name: Strings("card.player.name").local,
+            resizeToFit: self.resizeToFit
+        ) {
             CardRowView(
                 value: self.playerViewModel.armorPoints,
                 maxValue: self.playerViewModel.maxArmorPoints,
-                image: YonderIcons.armorPointsIcon)
+                image: YonderIcons.armorPointsIcon
+            )
             
             CardRowView(
                 value: self.playerViewModel.health,
+                valueColor: self.playerHasCriticalHealth ? YonderColors.criticalHealth : nil,
                 maxValue: self.playerViewModel.maxHealth,
-                image: YonderIcons.healthIcon)
+                image: YonderIcons.healthIcon
+            )
             
             CardRowView(
                 prefix: Strings("currencySymbol").local,
                 value: self.playerViewModel.gold,
-                image: YonderIcons.goldIcon)
+                image: YonderIcons.goldIcon
+            )
         }
     }
 }
