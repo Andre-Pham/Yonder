d3802a0f9ad00e4445aa73db4ed75451127f6847
Andre Pham <andrepham33@gmail.com>
2024-03-26 14:15:38 +1100

    Introduced save manager to save after the player travels

--- codebase/yonder/yonder/Model Layer/Game/GameContext/GameContext.swift
+++ codebase/yonder/yonder/Model Layer/Game/GameContext/GameContext.swift
@@ -13,12 +13,14 @@ class GameContext: Storable {
     public let stableGameStateManager: StableGameStateManager
     public let playerStageManager: PlayerStageManager
     public let contentManager: ContentManager
+    private let saveManager: SaveManager
     
     init() {
         self.turnManager = TurnManager()
         self.stableGameStateManager = StableGameStateManager()
         self.playerStageManager = PlayerStageManager(stage: 0)
         self.contentManager = ContentManager() // Must subscribe after playerStageManager so stage updates before content
+        self.saveManager = SaveManager()
     }
     
     // MARK: - Serialisation
@@ -35,6 +37,7 @@ class GameContext: Storable {
         self.stableGameStateManager = dataObject.getObject(Field.stableGameStateManager.rawValue, type: StableGameStateManager.self)
         self.playerStageManager = dataObject.getObject(Field.playerStageManager.rawValue, type: PlayerStageManager.self)
         self.contentManager = dataObject.getObject(Field.contentManager.rawValue, type: ContentManager.self)
+        self.saveManager = SaveManager()
     }
 
     func toDataObject() -> DataObject {
---
+++ codebase/yonder/yonder/Model Layer/Game/GameContext/SaveManager.swift
@@ -0,0 +1,34 @@
+//
+//  SaveManager.swift
+//  yonder
+//
+//  Created by Andre Pham on 26/3/2024.
+//
+
+import Foundation
+
+/// Manages save triggers that occur relating to game actions.
+/// Subscribes to certain game events to detect when an automatic quick-save should be done.
+/// Does NOT manage all game saves. Only those related to game actions.
+/// Player quick-saves in settings? NOT SaveManager.
+/// Player closes or refreshes the app? NOT SaveManager.
+/// Player does something in-game, and we want to save in the background while the user plays in case of a crash or fatal error? This is what this is for.
+class SaveManager: AfterPlayerTravelSubscriber {
+    
+    init() {
+        AfterPlayerTravelPublisher.subscribe(self)
+    }
+    
+    func afterPlayerTravel(player: Player) {
+        DispatchQueue.global().async {
+            Session.instance.saveGame()
+        }
+    }
+    
+    // If you wanted to be REALLY thorough with saving, how would you go about it?
+    // You would save every time a stable game state was reached
+    // (See StableGameStateManager)
+    // However that's way overkill - even this class is only for in the case of a fatal crash or device shutdown
+    // For 99.9% of cases we rely on Session automatically saving when the app is exited (or moves to the background, etc.)
+    
+}
